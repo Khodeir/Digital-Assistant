1d5f42c0e35c0d42468074ae9b3ba7ed59d73c4d
Mohamed Khodeir <m.khodeir@mail.utoronto.ca>
2015-06-26 06:40:15 +0200

    start day at 00

--- static/services.js
+++ static/services.js
@@ -46,7 +46,7 @@ app.service('History', function ($http) {
   this.getTimeSlots = function(){
     var timeslots = [];
     // POPULATE TIMESLOT LIST.. PRETTY UGLY
-    var initial_hour = 8;
+    var initial_hour = 0;
     for(var i = 0; i<48; i++){
       var timeslot = new Date(today);
       timeslot.setHours(initial_hour+(i/2) | 0);
